0e168f77a8246eaedfbf214302e5b6fc8de829e2
denis_adamchuk <denis.adamchuk@gmail.com>
2019-07-15 09:43:24 +0300

    Split methods in more methods to improve readability and avoid PVS alarms

--- mrCore/RefsToLinesMatcher.cs
+++ mrCore/RefsToLinesMatcher.cs
@@ -45,45 +45,82 @@ namespace mrCore
       {
          Debug.Assert(difftoolInfo.Left.HasValue || difftoolInfo.Right.HasValue);
 
+         string oldPath = String.Empty;
+         string newPath = String.Empty;
+         getPositionPaths(ref difftoolInfo, ref oldPath, ref newPath);
+
+         string oldLine = String.Empty;
+         string newLine = String.Empty;
+         getPositionLines(state, ref difftoolInfo, ref oldLine, ref newLine);
+
          Position position = new Position
          {
             Refs = diffRefs,
-
-            // GitLab expects two filenames in all cases and the only case when they are different is file rename.
-            OldPath = difftoolInfo.Left?.FileName ?? difftoolInfo.Right.Value.FileName,
-            NewPath = difftoolInfo.Right?.FileName ?? difftoolInfo.Left.Value.FileName
+            OldPath = oldPath,
+            OldLine = oldLine,
+            NewPath = newPath,
+            NewLine = newLine
          };
+         return position;
+      }
 
+      private void getPositionPaths(ref DiffToolInfo difftoolInfo, ref string oldPath, ref string newPath)
+      {
+         if (difftoolInfo.Left.HasValue && !difftoolInfo.Right.HasValue)
+         {
+            // When a file is removed, diff tool does not provide a right-side file name, but GitLab needs both
+            oldPath = difftoolInfo.Left.Value.FileName;
+            newPath = oldPath;
+         }
+         else if (!difftoolInfo.Left.HasValue && difftoolInfo.Right.HasValue)
+         {
+            // When a file is added, diff tool does not provide a left-side file name, but GitLab needs both
+            oldPath = difftoolInfo.Right.Value.FileName;
+            newPath = oldPath;
+         }
+         else if (difftoolInfo.Left.HasValue && difftoolInfo.Right.HasValue)
+         {
+            // Filenames may be different and may be the same, it does not matter here, provide them both to GitLab
+            oldPath = difftoolInfo.Left.Value.FileName;
+            newPath = difftoolInfo.Right.Value.FileName;
+         }
+         else
+         {
+            Debug.Assert(false);
+         }
+      }
+
+      private void getPositionLines(MatchResult state, ref DiffToolInfo difftoolInfo,
+         ref string oldLine, ref string newLine)
+      {
          switch (state)
          {
             case MatchResult.NewLineOnly:
                Debug.Assert(difftoolInfo.Right.HasValue);
 
-               position.OldLine = null;
-               position.NewLine = difftoolInfo.Right.Value.LineNumber.ToString();
+               oldLine = null;
+               newLine = difftoolInfo.Right.Value.LineNumber.ToString();
                break;
 
             case MatchResult.OldLineOnly:
                Debug.Assert(difftoolInfo.Left.HasValue);
 
-               position.OldLine = difftoolInfo.Left.Value.LineNumber.ToString();
-               position.NewLine = null;
+               oldLine = difftoolInfo.Left.Value.LineNumber.ToString();
+               newLine = null;
                break;
 
             case MatchResult.Both:
                Debug.Assert(difftoolInfo.Right.HasValue);
                Debug.Assert(difftoolInfo.Left.HasValue);
 
-               position.OldLine = difftoolInfo.Left.Value.LineNumber.ToString();
-               position.NewLine = difftoolInfo.Right.Value.LineNumber.ToString();
+               oldLine = difftoolInfo.Left.Value.LineNumber.ToString();
+               newLine = difftoolInfo.Right.Value.LineNumber.ToString();
                break;
 
             case MatchResult.Undefined:
                Debug.Assert(false);
                break;
          }
-
-         return position;
       }
 
       private MatchResult match(DiffRefs diffRefs, DiffToolInfo difftoolInfo)
@@ -92,61 +129,77 @@ namespace mrCore
          GitDiffAnalyzer gitDiffAnalyzer = new GitDiffAnalyzer(_gitRepository,
             diffRefs.BaseSHA, diffRefs.HeadSHA,
             difftoolInfo.Left?.FileName ?? null, difftoolInfo.Right?.FileName ?? null);
-        
-         // First, check if we're at the right side
-         if (!difftoolInfo.IsLeftSideCurrent)
-         {
-            Debug.Assert(difftoolInfo.Right.HasValue);
 
-            // If we are at the right side, check if a selected line was added/modified
-            if (gitDiffAnalyzer.IsLineAddedOrModified(difftoolInfo.Right.Value.LineNumber))
-            {
-               return MatchResult.NewLineOnly;
-            }
-            // Bad line number, a line belongs to the right side but it is not added
-            else if (!difftoolInfo.Left.HasValue)
-            {
-               return MatchResult.Undefined;
-            }
-            // If selected line is not added/modified, we need to send a deleted line to Gitlab
-            // Make sure that a line selected at the left side was deleted
-            else if (gitDiffAnalyzer.IsLineDeleted(difftoolInfo.Left.Value.LineNumber))
-            {
-               return MatchResult.OldLineOnly;
-            }
+         MatchResult result = MatchResult.Undefined;
+         if (!difftoolInfo.IsLeftSideCurrent && matchRightSide(difftoolInfo, gitDiffAnalyzer, out result))
+         {
+            return result;
          }
-         else
+         else if (difftoolInfo.IsLeftSideCurrent && matchLeftSide(difftoolInfo, gitDiffAnalyzer, out result))
          {
-            Debug.Assert(difftoolInfo.Left.HasValue);
-
-            // If we are the left side, let's check first if the selected line was deleted
-            if (gitDiffAnalyzer.IsLineDeleted(difftoolInfo.Left.Value.LineNumber))
-            {
-               return MatchResult.OldLineOnly;
-            }
-            // Bad line number, a line belongs to the left side but it is not removed
-            else if (!difftoolInfo.Right.HasValue)
-            {
-               return MatchResult.Undefined;
-            }
-            // If selected line was not deleted, check a right-side line number
-            // Make sure that it was added/modified
-            else if (gitDiffAnalyzer.IsLineAddedOrModified(difftoolInfo.Right.Value.LineNumber))
-            {
-               return MatchResult.NewLineOnly;
-            }
+            return result;
          }
-
-         // If neither left nor right lines are neither deleted nor added/modified,
-         // then the only acceptable way is that they are unchanged. Check if they are equal.
-         // If they are not, fallback.
-         if (checkIfLinesAreEqual(diffRefs, difftoolInfo))
+         else if (checkIfLinesAreEqual(diffRefs, difftoolInfo))
          {
+            // If neither left nor right lines are neither deleted nor added/modified,
+            // then the only acceptable way is that they are unchanged. Check if they are equal.
             return MatchResult.Both;
          }
 
          // Difftool info is invalid
          return MatchResult.Undefined;
+      }
+
+      bool matchRightSide(DiffToolInfo difftoolInfo, GitDiffAnalyzer gitDiffAnalyzer, out MatchResult result)
+      {
+         Debug.Assert(difftoolInfo.Right.HasValue);
+
+         result = MatchResult.Undefined;
+
+         // If we are at the right side, check if a selected line was added/modified
+         if (gitDiffAnalyzer.IsLineAddedOrModified(difftoolInfo.Right.Value.LineNumber))
+         {
+            result = MatchResult.NewLineOnly;
+         }
+         // Bad line number, a line belongs to the right side but it is not added
+         else if (!difftoolInfo.Left.HasValue)
+         {
+            result = MatchResult.Undefined;
+         }
+         // If selected line is not added/modified, we need to send a deleted line to Gitlab
+         // Make sure that a line selected at the left side was deleted
+         else if (gitDiffAnalyzer.IsLineDeleted(difftoolInfo.Left.Value.LineNumber))
+         {
+            result = MatchResult.OldLineOnly;
+         }
+
+         return result != MatchResult.Undefined;
+      }
+
+      bool matchLeftSide(DiffToolInfo difftoolInfo, GitDiffAnalyzer gitDiffAnalyzer, out MatchResult result)
+      {
+         Debug.Assert(difftoolInfo.Left.HasValue);
+
+         result = MatchResult.Undefined;
+
+         // If we are the left side, let's check first if the selected line was deleted
+         if (gitDiffAnalyzer.IsLineDeleted(difftoolInfo.Left.Value.LineNumber))
+         {
+            result = MatchResult.OldLineOnly;
+         }
+         // Bad line number, a line belongs to the left side but it is not removed
+         else if (!difftoolInfo.Right.HasValue)
+         {
+            result = MatchResult.Undefined;
+         }
+         // If selected line was not deleted, check a right-side line number
+         // Make sure that it was added/modified
+         else if (gitDiffAnalyzer.IsLineAddedOrModified(difftoolInfo.Right.Value.LineNumber))
+         {
+            result = MatchResult.NewLineOnly;
+         }
+
+         return result != MatchResult.Undefined;
       }
 
       private bool checkIfLinesAreEqual(DiffRefs diffRefs, DiffToolInfo info)
