8d67d2b7ea06c67e1a036355f19460dbd25bb2de
denis_adamchuk <denis.adamchuk@gmail.com>
2019-07-28 09:35:39 +0300

    Increment version

---
+++ build/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("['0.4.8.0']")]
+[assembly: AssemblyFileVersion("['0.4.8.0']")]
