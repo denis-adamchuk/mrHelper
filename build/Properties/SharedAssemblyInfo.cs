ca858b9877e645a0bd341a0b2623d64cd782b757
denis_adamchuk <denis.adamchuk@gmail.com>
2019-07-28 09:58:11 +0300

    Update .gitignore

--- build/Properties/SharedAssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyVersion("['0.4.8.0']")]
-[assembly: AssemblyFileVersion("['0.4.8.0']")]
