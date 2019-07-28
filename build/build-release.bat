4be96a61c48c9dc8dd34d94e25f4aa61693658ec
denis_adamchuk <denis.adamchuk@gmail.com>
2019-07-28 09:53:40 +0300

    Add MSBuild batch scripts

---
+++ build/build-release.bat
@@ -0,0 +1,1 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" && msbuild /v:q /property:GenerateFullPaths=true /t:Build /p:Configuration="Release" /m:6 ../mrHelper.sln
