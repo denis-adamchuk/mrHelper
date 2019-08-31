f4dd72c634c33c6316de66211efa17db0b86c50d
denis_adamchuk <denis.adamchuk@gmail.com>
2019-08-31 16:09:48 +0300

    Add support of MSI to the build script

---
+++ build/build-install.bat
@@ -0,0 +1,2 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
+devenv ../mrHelper.sln /build Install
--- build/make_build.py
+++ build/make_build.py
@@ -1,6 +1,7 @@
 import os
 import re
 import sys
+import uuid
 import zipfile
 import argparse
 import traceback
@@ -18,12 +19,19 @@ class CommandLineArgs:
    def __init__(self, argv):
       parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,\
          description = 'mrHelper build script')
+
       versionHelp = 'Version number (e.g. 0.4.6.0)'
       parser.add_argument('-v', '--version', dest='version', nargs=1, help=versionHelp)
+
       pushHelp = 'Push incremented version to remote'
       parser.add_argument('-p', '--push', action='store_true', help=pushHelp)
+
       configHelp = 'Configuration filename with path'
       parser.add_argument('-c', '--config', dest='config', nargs=1, help=configHelp)
+
+      msiHelp = 'Create MSI (if not specified, creates a zip)'
+      parser.add_argument('-m', '--msi', action='store_true', help=msiHelp)
+
       self.args = parser.parse_known_args(argv)[0]
 
       if not self.args.version:
@@ -43,6 +51,9 @@ class CommandLineArgs:
 
    def config(self):
       return self.args.config[0]
+
+   def msi(self):
+      return self.args.msi
 
    def push(self):
       return self.args.push
@@ -80,12 +91,24 @@ class ScriptConfigParser:
    def version_file(self):
       return self.config.get('Version', 'AssemblyInfo')
 
+   def installer_project(self):
+      return self.config.get('Installer', 'project')
+
+   def msi_original_name(self):
+      return self.config.get('Installer', 'msi_original_name')
+
+   def msi_target_name_template(self):
+      return self.config.get('Installer', 'msi_target_name_template')
+
    def _initialize(self, filename):
       self._addOption('Path', 'repository', '.')
       self._addOption('Path', 'Extras', 'extras')
       self._addOption('Path', 'Bin', 'bin')
       self._addOption('Path', 'BuildScript', 'build/build-release.bat')
       self._addOption('Version', 'AssemblyInfo', 'Properties/SharedAssemblyInfo.cs')
+      self._addOption('Installer', 'project', '')
+      self._addOption('Installer', 'msi_original_name', '')
+      self._addOption('Installer', 'msi_target_name_template', '')
 
       with open(filename, 'w') as configFile:
          self.config.write(configFile)
@@ -102,6 +125,7 @@ class ScriptConfigParser:
       self._validatePathInConfig(self.config, 'Path', 'Bin')
       self._validateFileInConfig(self.config, 'Path', 'BuildScript')
       self._validateFileInConfig(self.config, 'Version', 'AssemblyInfo')
+      self._validateFileInConfig(self.config, 'Installer', 'project')
 
    def _validatePathInConfig(self, config, section, option):
       path = self.config.get(section, option)
@@ -112,6 +136,72 @@ class ScriptConfigParser:
       path = self.config.get(section, option)
       if not os.path.exists(path) or not os.path.isfile(path):
          raise self.Exception(f'Bad file "{path}"')
+
+
+class PreBuilder:
+   """ PreBuilder - Performs pre-build steps
+
+   """
+   class Exception(RuntimeError):
+      pass
+
+   def __init__(self, version, installer_project):
+      self.version = version
+
+      if not os.path.exists(installer_project) or not os.path.isfile(installer_project):
+         raise self.Exception(f'Bad installer project file "{installer_project}"')
+      self.installer_project = installer_project
+
+   def prebuild(self):
+      self._write_version()
+
+   def _write_version(self):
+      version_rex = re.compile(r'(\s*\"ProductVersion\"\s*=\s*\"8)(?:\:[0-9\.]+\")')
+      product_code_rex = re.compile(r'(\s*\"ProductCode\"\s*=\s*\"8:)(?:{[0-9A-F\-]+})(\")')
+
+      lines = []
+      with open (self.installer_project, 'r') as f:
+         for line in f:
+            if re.match(version_rex, line):
+               lines.append(version_rex.sub(r'\1:{0}"'.format(self.version[:-2]), line))
+            elif re.match(product_code_rex, line):
+               guid = str(uuid.uuid4())
+               guid = guid.upper()
+               print(guid)
+               lines.append(product_code_rex.sub(r'\1{{{0}}}\2'.format(guid), line))
+            else:
+               lines.append(line)
+
+      with open(self.installer_project, 'w') as f:
+         for line in lines:
+            f.write(line)
+
+
+class PostBuilder:
+   """ PostBuilder - Performs post-build steps
+
+   """
+   class Exception(RuntimeError):
+      pass
+
+   def __init__(self, version, bin_path, msi_name, msi_target_name_template):
+      self.version = version
+
+      msi_path = os.path.join(bin_path, msi_name)
+      if not os.path.exists(msi_path) or not os.path.isfile(msi_path):
+         raise self.Exception(f'Bad MSI file "{msi_path}"')
+      self.msi_path = msi_path
+      self.bin_path = bin_path
+      self.msi_target_name_template = msi_target_name_template
+
+   def postbuild(self):
+      self._rename_msi()
+
+   def _rename_msi(self):
+      msi_new_name = self.msi_target_name_template
+      msi_new_name = msi_new_name.replace('{Version}', self.version)
+      msi_new_path = os.path.join(self.bin_path, msi_new_name)
+      os.rename(self.msi_path, msi_new_path)
 
 
 class Builder:
@@ -269,11 +359,20 @@ try:
 
    config = ScriptConfigParser(args.config())
 
+   if args.msi():
+      prebuilder = PreBuilder(args.version(), config.installer_project())
+      prebuilder.prebuild()
+
    builder = Builder(args.version(), config.version_file(), config.build_script())
    builder.build()
 
-   maker = PackageMaker(args.version(), config.bin(), config.extras())
-   maker.make_package()
+   if args.msi():
+      postbuilder = PostBuilder(args.version(), config.bin(), config.msi_original_name(), config.msi_target_name_template())
+      postbuilder.postbuild()
+
+   if not args.msi():
+      maker = PackageMaker(args.version(), config.bin(), config.extras())
+      maker.make_package()
 
    if args.push():
       repository = RepositoryHelper(config.repository())
@@ -284,6 +383,10 @@ except ScriptConfigParser.Exception as e:
    handle_error(1, e, "validating config", False, False, False)
 except CommandLineArgs.Exception as e:
    handle_error(2, e, "parsing command-line", False, False, False)
+except PreBuilder.Exception as e:
+   handle_error(6, e, "pre-build step", False, False, False)
+except PreBuilder.Exception as e:
+   handle_error(7, e, "post-build step", False, True, False)
 except Builder.Exception as e:
    handle_error(3, e, "preparing to build", False, False, False)
 except PackageMaker.Exception as e:
