cbdf9d3eca7e6ad1f0820dad18500339726e173d
denis_adamchuk <denis.adamchuk@gmail.com>
2019-12-04 23:42:30 +0300

    Add ability to create BCPreferences.XML if it does not exist yet
    Closes #160

--- src/DiffTool/src/BC3Tool.cs
+++ src/DiffTool/src/BC3Tool.cs
@@ -22,13 +22,12 @@ namespace mrHelper.DiffTool
 
       public string GetToolName()
       {
-         return "Beyond Compare 3";
+         return ToolName;
       }
 
       public string[] GetToolRegistryNames()
       {
-         string[] names = { "Beyond Compare 3", "Beyond Compare Version 3"};
-         return names;
+         return ToolRegistryNames;
       }
 
       /// <summary>
@@ -38,103 +37,159 @@ namespace mrHelper.DiffTool
       /// </summaryArgument>
       public void PatchToolConfig(string launchCommand)
       {
-         var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-         var bcFolder = System.IO.Path.Combine(appData, "Scooter Software", "Beyond Compare 3");
-         var prefs = System.IO.Path.Combine(bcFolder, "BCPreferences.xml");
-         if (!System.IO.File.Exists(prefs))
-         {
-            throw new DiffToolIntegrationException(String.Format("File is missing: \"{0}\"", prefs));
-         }
-
-         string integrationKey = "mrhelper-bc3-integration";
-         string defaultShortcut = "32843"; // Alt-K
-         var arguments = " %22%25f1%22 %l1 %22%25f2%22";
+         string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+         string configFilePath = System.IO.Path.Combine(appData, ToolCompanyName, ToolName, ConfigFileName);
 
          XmlDocument document = new XmlDocument();
-         document.Load(prefs);
-         var root = document.SelectSingleNode("BCPreferences");
-         if (root == null)
+         if (System.IO.File.Exists(configFilePath))
          {
-            throw new DiffToolIntegrationException(
-               String.Format("Unexpected format of preferences file \"{0}\". Missing \"BCPreferences\" node", prefs));
+            document.Load(configFilePath);
+         }
+         else
+         {
+            XmlNode docNode = document.CreateXmlDeclaration("1.0", "UTF-8", null);
+            document.AppendChild(docNode);
+
+            XmlNode comment = document.CreateComment(ConfigFileComment);
+            document.AppendChild(comment);
          }
 
-         var tbPrefs = root.SelectSingleNode("TBcPrefs");
-         if (tbPrefs == null)
+         string fullCommand = launchCommand + LaunchArguments;
+         XmlNode openWiths = getAllApplicationRecords(document);
+         XmlNode appRecord = findOurApplicationRecord(openWiths);
+         if (appRecord != null)
          {
-            throw new DiffToolIntegrationException(
-               String.Format("Unexpected format of preferences file \"{0}\". Missing \"TBcPrefs\" node", prefs));
+            XmlNode currentCmdLine = appRecord.SelectSingleNode("CmdLine");
+            XmlNode currentShortCut = appRecord.SelectSingleNode("ShortCut");
+            Debug.Assert(currentCmdLine != null && currentShortCut != null);
+
+            ((XmlElement)currentCmdLine).SetAttribute("Value", fullCommand);
+            ((XmlElement)currentShortCut).SetAttribute("Value", DefaultShortcut);
+
+            Trace.TraceInformation(String.Format(
+               "Updated \"{0}\" file. CmdLine=\"{1}\". ShortCut=\"{2}\"",
+               configFilePath, fullCommand, DefaultShortcut));
+
+            document.Save(configFilePath);
+            return;
          }
 
-         var opensWith = tbPrefs.SelectSingleNode("OpenWiths");
-         if (opensWith == null)
-         {
-            throw new DiffToolIntegrationException(
-               String.Format("Unexpected format of preferences file \"{0}\". Missing \"opensWith\" node", prefs));
-         }
+         int id = getAllApplicationRecordIds(openWiths).DefaultIfEmpty(-1 /* to obtain Id = 0 */).Max() + 1;
+         XmlNode newNode = document.CreateElement("_" + id.ToString());
 
-         // check if we already integrated
-         List<int> ids = new List<int>();
-         foreach (XmlNode child in opensWith.ChildNodes)
-         {
-            ids.Add(int.Parse(child.Name.TrimStart('_')));
-            var currentDescription = child.SelectSingleNode("Description");
-            if (currentDescription != null)
-            {
-               var value = currentDescription.Attributes.GetNamedItem("Value");
-               if (value.Value == integrationKey)
-               {
-                  // seems to be already patched but let's update it 
-                  var currentCmdLine = child.SelectSingleNode("CmdLine");
-                  var currentShortCut = child.SelectSingleNode("ShortCut");
-                  if (currentCmdLine == null || currentShortCut == null)
-                  {
-                     Trace.TraceWarning(
-                        String.Format("\"{0}\" configuration file is already patched, but {1} is missing.",
-                                      prefs, (currentCmdLine == null ? "CmdLine" : "ShortCut")));
+         XmlElement cmdLine = document.CreateElement("CmdLine");
+         cmdLine.SetAttribute("Value", fullCommand);
 
-                     opensWith.RemoveChild(child);
-                     continue;
-                  }
+         XmlElement description = document.CreateElement("Description");
+         description.SetAttribute("Value", IntegrationKey);
 
-                  ((XmlElement)currentCmdLine).SetAttribute("Value", launchCommand + arguments);
-                  ((XmlElement)currentShortCut).SetAttribute("Value", defaultShortcut);
-                  document.Save(prefs);
-
-                  Trace.TraceInformation(String.Format(
-                     "Updated \"{0}\" file. CmdLine=\"{1}\". ShortCut=\"{2}\"",
-                     prefs, launchCommand + arguments, defaultShortcut));
-
-                  return;
-               }
-            }
-         }
-
-         ids.Sort();
-         int id = ids.Count > 0 ? ids.Last() + 1 : 1;
-         var newNode = document.CreateElement("_" + id.ToString());
-
-         var cmdLine = document.CreateElement("CmdLine");
-         cmdLine.SetAttribute("Value", launchCommand + arguments);
-
-         var description = document.CreateElement("Description");
-         description.SetAttribute("Value", integrationKey);
-
-         var shortcut = document.CreateElement("ShortCut");
-         shortcut.SetAttribute("Value", defaultShortcut);
+         XmlElement shortcut = document.CreateElement("ShortCut");
+         shortcut.SetAttribute("Value", DefaultShortcut);
 
          newNode.AppendChild(cmdLine);
          newNode.AppendChild(description);
          newNode.AppendChild(shortcut);
-         opensWith.AppendChild(newNode);
+         openWiths.AppendChild(newNode);
 
          Trace.TraceInformation(String.Format(
             "Patched \"{0}\" file. CmdLine=\"{1}\". Description=\"{2}\". ShortCut=\"{3}\"",
-            prefs, cmdLine.Value, description.Value, shortcut.Value));
+            configFilePath, fullCommand, IntegrationKey, DefaultShortcut));
 
-         document.Save(prefs);
+         document.Save(configFilePath);
       }
 
+      private static XmlNode getAllApplicationRecords(XmlDocument document)
+      {
+         XmlNode root = document.SelectSingleNode("BCPreferences");
+         if (root == null)
+         {
+            if (document.DocumentElement != null)
+            {
+               throw new DiffToolIntegrationException(String.Format("Wrong root element. Must be \"BCPreferences\" node."));
+            }
+
+            root = document.CreateElement("BCPreferences");
+            if (root == null)
+            {
+               throw new DiffToolIntegrationException(String.Format("Cannot create \"BCPreferences\" node"));
+            }
+            document.AppendChild(root);
+         }
+
+         XmlNode tbcPrefs = root.SelectSingleNode("TBcPrefs");
+         if (tbcPrefs == null)
+         {
+            tbcPrefs = document.CreateElement("TBcPrefs");
+            if (tbcPrefs == null)
+            {
+               throw new DiffToolIntegrationException(String.Format("Cannot create \"TBcPrefs\" node"));
+            }
+            root.AppendChild(tbcPrefs);
+         }
+
+         XmlNode openWiths = tbcPrefs.SelectSingleNode("OpenWiths");
+         if (openWiths == null)
+         {
+            openWiths = document.CreateElement("OpenWiths");
+            if (openWiths == null)
+            {
+               throw new DiffToolIntegrationException(String.Format("Cannot create \"OpenWiths\" node"));
+            }
+            tbcPrefs.AppendChild(openWiths);
+         }
+
+         return openWiths;
+      }
+
+      private static IEnumerable<int> getAllApplicationRecordIds(XmlNode openWiths)
+      {
+         return openWiths.ChildNodes
+            .Cast<XmlNode>()
+            .Select(x => int.TryParse(x.Name.TrimStart('_'), out int id) ? id : -1);
+      }
+
+      private static XmlNode findOurApplicationRecord(XmlNode openWiths)
+      {
+         // check if we already integrated
+         foreach (XmlNode child in openWiths.ChildNodes)
+         {
+            XmlNode currentDescription = child.SelectSingleNode("Description");
+            if (currentDescription != null)
+            {
+               XmlNode value = currentDescription.Attributes.GetNamedItem("Value");
+               if (value.Value == IntegrationKey)
+               {
+                  // seems to be already patched but let's update it 
+                  XmlNode currentCmdLine = child.SelectSingleNode("CmdLine");
+                  XmlNode currentShortCut = child.SelectSingleNode("ShortCut");
+                  if (currentCmdLine == null || currentShortCut == null)
+                  {
+                     Trace.TraceWarning(
+                        String.Format("Configuration file is already patched, but {0} is missing.",
+                                      (currentCmdLine == null ? "CmdLine" : "ShortCut")));
+
+                     openWiths.RemoveChild(child);
+                     continue;
+                  }
+
+                  return child;
+               }
+            }
+         }
+
+         return null;
+      }
+
+      private readonly static string IntegrationKey = "mrhelper-bc3-integration";
+      private readonly static string DefaultShortcut = "32843"; // Alt-K
+      private readonly static string LaunchArguments = " %22%25f1%22 %l1 %22%25f2%22";
+
+      private readonly static string ToolName = "Beyond Compare 3";
+      private readonly static string[] ToolRegistryNames = { ToolName, "Beyond Compare Version 3" };
+      private readonly static string ToolCompanyName = "Scooter Software";
+      private readonly static string ConfigFileName = "BCPreferences.xml";
+      private readonly static string ConfigFileComment =
+         String.Format(" Produced by {0} from {1} ", ToolName, ToolCompanyName);
    }
 }
 
