a9aacfda00446b661e3e2d0fad5625ece6bc8c4b
denis_adamchuk <denis.adamchuk@gmail.com>
2020-03-16 11:18:52 +0300

    Increment version

--- src/Properties/SharedAssemblyInfo.cs
+++ src/Properties/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.7.8.0")]
-[assembly: AssemblyFileVersion("1.7.8.0")]
+[assembly: AssemblyVersion("1.7.9.0")]
+[assembly: AssemblyFileVersion("1.7.9.0")]
