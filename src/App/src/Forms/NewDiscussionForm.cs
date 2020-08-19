6084f9de7d567c4b4b934c79b6b479f887eb8325
denis_adamchuk <denis.adamchuk@gmail.com>
2020-08-19 13:28:41 +0300

    Implement Insert Code button

--- src/App/src/Forms/NewDiscussionForm.cs
+++ src/App/src/Forms/NewDiscussionForm.cs
@@ -45,6 +45,34 @@ namespace mrHelper.App.Forms
          }
       }
 
+      private void buttonInsertCode_Click(object sender, EventArgs e)
+      {
+         insertCodePlaceholderIntoTextBox(textBoxDiscussionBody);
+         textBoxDiscussionBody.Focus();
+      }
+
+      private static void insertCodePlaceholderIntoTextBox(System.Windows.Controls.TextBox textBox)
+      {
+         int selectionStartIndex = textBox.SelectionStart;
+         int selectionLength = textBox.SelectionLength;
+         int selectionEndIndex = selectionStartIndex + selectionLength - 1; // less than start index if nothing selected
+
+         string text = textBox.Text;
+         bool addNewLineBeforeCursor = selectionStartIndex > 0 && text[selectionStartIndex - 1] != '\n';
+         bool addNewLineAfterCursor = selectionEndIndex < text.Length - 1 && text[selectionEndIndex + 1] != '\r';
+
+         string afterCursorText = String.Format("\r\n```{0}", addNewLineAfterCursor ? "\r\n" : String.Empty);
+         string beforeCursorText = String.Format("{0}```\r\n", addNewLineBeforeCursor ? "\r\n" : String.Empty);
+
+         int beforeCursorTextPos = Math.Max(0, selectionStartIndex);
+         int afterCursorTextPos = beforeCursorTextPos + beforeCursorText.Length + selectionLength;
+
+         textBox.Text = textBox.Text.Insert(beforeCursorTextPos, beforeCursorText);
+         textBox.Text = textBox.Text.Insert(afterCursorTextPos, afterCursorText);
+
+         textBox.SelectionStart = beforeCursorTextPos + beforeCursorText.Length;
+      }
+
       private void NewDiscussionForm_Shown(object sender, EventArgs e)
       {
          Win32Tools.ForceWindowIntoForeground(this.Handle);
