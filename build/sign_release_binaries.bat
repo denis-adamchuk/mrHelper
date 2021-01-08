413dc497986d405c102ae1cd37a77dd81b5fb4f5
denis_adamchuk <denis.adamchuk@gmail.com>
2021-01-08 22:34:17 +0300

    Change timestamp dll path (old server no longer works)

--- build/sign_release_binaries.bat
+++ build/sign_release_binaries.bat
@@ -3,16 +3,16 @@ call ".\set-env.bat"
 cd ..\bin\Release
 
 for %%f in (mrHelper*.exe) do (
-   signtool sign /a /t http://timestamp.verisign.com/scripts/timstamp.dll %%f
+   signtool sign /a /t http://timestamp.globalsign.com/scripts/timstamp.dll %%f
    if %ERRORLEVEL% NEQ 0 EXIT /B 1
 )
 
 for %%f in (mrHelper*.dll) do (
-   signtool sign /a /t http://timestamp.verisign.com/scripts/timstamp.dll %%f
+   signtool sign /a /t http://timestamp.globalsign.com/scripts/timstamp.dll %%f
    if %ERRORLEVEL% NEQ 0 EXIT /B 1
 )
 
-signtool sign /a /t http://timestamp.verisign.com/scripts/timstamp.dll GitLabSharp.dll
+signtool sign /a /t http://timestamp.globalsign.com/scripts/timstamp.dll GitLabSharp.dll
 if %ERRORLEVEL% NEQ 0 EXIT /B 1
 
 cd ..\..\build
