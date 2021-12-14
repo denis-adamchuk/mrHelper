28e1a742429d0c0d4520754a827eea55b6b2813d
denis_adamchuk <denis.adamchuk@gmail.com>
2021-12-14 18:49:18 +0300

    Fix conversion of absolute timestamps to relative ones
    Closes #521

--- src/Common/Tools/TimeUtils.cs
+++ src/Common/Tools/TimeUtils.cs
@@ -24,12 +24,9 @@ namespace mrHelper.Common.Tools
 
       public static string DateTimeToStringAgo(DateTime dateTime)
       {
-         return TimeSpanToStringAgo(DateTime.Now - dateTime.ToLocalTime());
-      }
-
-      public static string TimeSpanToStringAgo(TimeSpan timeSpan)
-      {
-         string timeSpanAsText = TimeSpanToString(timeSpan);
+         DateTime laterTime = DateTime.Now;
+         DateTime earlyTime = dateTime.ToLocalTime();
+         string timeSpanAsText = TimeSpanToString(laterTime, earlyTime);
          if (timeSpanAsText == DayTimeSpanText)
          {
             return YesterdayText;
@@ -41,8 +38,9 @@ namespace mrHelper.Common.Tools
          return String.Format("{0} {1}", timeSpanAsText, "ago");
       }
 
-      public static string TimeSpanToString(TimeSpan timeSpan)
+      public static string TimeSpanToString(DateTime laterTime, DateTime earlyTime)
       {
+         var timeSpan = laterTime - earlyTime;
          if (timeSpan.Seconds >= 50)
          {
             timeSpan = timeSpan.Add(new TimeSpan(0, 1, 0));
@@ -73,7 +71,6 @@ namespace mrHelper.Common.Tools
 
          double totalMonths = Math.Floor(timeSpan.TotalDays / 30);
          double totalWeeks = Math.Floor(timeSpan.TotalDays / 7);
-         double totalDays = Math.Floor(timeSpan.TotalDays);
          double totalHours = Math.Floor(timeSpan.TotalHours);
          double totalMinutes = Math.Floor(timeSpan.TotalMinutes);
          if (totalMonths > 0)
@@ -84,13 +81,16 @@ namespace mrHelper.Common.Tools
          {
             return formatNumber(totalWeeks, WeekText);
          }
-         else if (totalDays > 0)
-         {
-            return formatNumber(totalDays, DayText);
-         }
          else if (totalHours > 0)
          {
-            return formatNumber(totalHours, HourText);
+            // It is bad from UX perspective to treat spans within a range [24h;48h) as 1 day back
+            // Let's check real distance in days.
+            DateTime laterTimeRounded = new DateTime(laterTime.Year, laterTime.Month, laterTime.Day);
+            DateTime earlyTimeRounded = new DateTime(earlyTime.Year, earlyTime.Month, earlyTime.Day);
+            double totalDays = Math.Floor((laterTimeRounded - earlyTimeRounded).TotalDays);
+            double number = totalDays > 0 ? totalDays : totalHours;
+            string text = totalDays > 0 ? DayText : HourText;
+            return formatNumber(number, text);
          }
          else if (totalMinutes > 0)
          {
@@ -145,51 +145,6 @@ namespace mrHelper.Common.Tools
       private static readonly string DayText = "day";
       private static readonly string HourText = "hour";
       private static readonly string MinuteText = "minute";
-
-      /*
-            TimeSpanToString():
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 0, 0))          == "just now");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 0, 1))          == "just now");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 0, 35))         == "just now");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 0, 55))         == "a minute");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 0, 59))         == "a minute");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 1, 0))          == "a minute");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 1, 55))         == "2 minutes");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 2, 05))         == "2 minutes");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 2, 05))         == "2 minutes");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 20, 59))        == "21 minutes");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(0, 25, 05))        == "25 minutes");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 0, 0))          == "an hour");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 1, 1))          == "an hour");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 30, 0))         == "an hour");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 49, 0))         == "an hour");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 49, 49))        == "an hour");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 49, 50))        == "2 hours");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 50, 0))         == "2 hours");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 59, 59))        == "2 hours");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(2, 45, 0))         == "2 hours");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(23, 0, 0))         == "23 hours");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(23, 45, 0))        == "23 hours");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(23, 49, 50))       == "a day");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(23, 50, 0))        == "a day");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 0, 0, 0))       == "a day");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 16, 59, 59))    == "a day");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 23, 49, 50))    == "2 days");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(1, 23, 59, 50))    == "2 days");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(6, 23, 59, 50))    == "7 days");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(13, 23, 59, 50))   == "14 days");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(19, 23, 59, 50))   == "20 days");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(20, 23, 59, 50))   == "3 weeks");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(22, 23, 59, 50))   == "3 weeks");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(26, 23, 59, 50))   == "3 weeks");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(27, 0, 0, 0))      == "3 weeks");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(27, 23, 49, 49))   == "3 weeks");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(27, 23, 49, 50))   == "4 weeks");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(29, 0, 0, 0))      == "4 weeks");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(30, 0, 0, 0))      == "a month");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(59, 0, 0, 0))      == "a month");
-         Debug.Assert(TimeUtils.TimeSpanToString(new TimeSpan(59, 29, 50, 50))   == "2 months");
-      */
    }
 }
 
