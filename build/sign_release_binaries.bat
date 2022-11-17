4baefe6c0682a9830e6ae7dc987a6adbe650258a
denis_adamchuk <denis.adamchuk@gmail.com>
2022-11-17 11:32:54 +0300

    Sign Aga.Controls library which is not taken from NuGet now

--- build/sign_release_binaries.bat
+++ build/sign_release_binaries.bat
@@ -15,6 +15,9 @@ for %%f in (mrHelper*.dll) do (
 signtool sign /a /tr http://timestamp.digicert.com GitLabSharp.dll
 if %ERRORLEVEL% NEQ 0 EXIT /B 1
 
+signtool sign /a /tr http://timestamp.digicert.com Aga.Controls.dll
+if %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 cd ..\..\build
 
 EXIT /B 0
